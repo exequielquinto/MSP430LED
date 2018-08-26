2bc2b66dd0ffd293a8779ad3d67cb186a9b56cd9
exequielquinto <42700497+exequielquinto@users.noreply.github.com>
2018-08-26 07:59:55 -0600

    Add files via upload

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <msp430g2533.h>//<msp430.h>                //<msp430g2533.h>
+// add comment
+
+
+#define RED BIT0
+#define GREEN BIT6
+
+/**
+ * blink.c
+ */
+void main(void)
+{
+    WDTCTL = WDTPW + WDTHOLD;       // stop watchdog timer
+    P1DIR |= RED + GREEN;           // configure P1.0 and _1.6 as output
+
+    P1OUT |= RED;                   //Inititally turn red ON
+    P1OUT &= ~GREEN;                //Initially turn green off
+    volatile unsigned int i;        // volatile to prevent optimization
+
+    while(1)
+    {
+
+        for(i=60000; i>0; i--);     // delay
+        P1OUT ^= (RED+GREEN);               // toggle P1.0
+    }
+}
